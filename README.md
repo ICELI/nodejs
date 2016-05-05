e863ad3d6c2b8a604c9d7b30b2123c747a566627
ICELI <woticeli@gmail.com>
2016-05-05 17:53:43 +0800

    遍历文件夹获取所有文件列表

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
-# nodejs-nodejs
+# nodejs
 learn nodejs
 
 ##nodejs-readFile
-###读取远程css里面引用的图片
+###readFile.js 读取远程css里面引用的图片
 * 读取css文件保存到本地
 * 匹配url(...)
 * 解析图片相对路径获取远程完整url
 * 获取图片保存到本地，相对css文件创建文件夹
+
+###getAllFiles.js 遍历文件夹获取所有文件列表
+* 遍历当前文件夹
+* 非文件夹即为文件直接返回目录
+* 文件夹继续遍历该文件夹
---
+++ nodejs-readFile/getAllFiles.js
@@ -0,0 +1,41 @@
+/**
+ * Created by iceli on 2016/5/5.
+ * 打开命令行工具，进入getAllFiles.js所在目录，输入：node getAllFiles.js '这里为你要统计的目标文件夹的目录'
+ * node getAllFiles.js ../
+ */
+
+var fs = require('fs');
+var root_path = process.argv[2]; // 命令行后第三个参数
+var w_file = 'res.lst';
+
+function getAllFiles(root) {
+    var res = [], files = fs.readdirSync(root);
+
+    files.forEach(function (file) {
+        var pathname = root + '/' + file
+            , stat = fs.lstatSync(pathname);
+
+        if (!stat.isDirectory()) {
+            res.push(pathname.replace(root_path, '.'));
+        } else {
+            res = res.concat(getAllFiles(pathname));
+        }
+    });
+    return res;
+}
+var w_content = getAllFiles(root_path).join('\n');
+fs.readFile(root_path + w_file, function (err, data) {
+    if (err && err.errno == 33) {
+        fs.open(w_file, "w", 0666, function (e, fd) {
+            if (e) throw e;
+            fs.write(fd, w_content, 0, 'utf8', function (e) {
+                if (e) throw e;
+                fs.closeSync(fd);
+            })
+        });
+    } else {
+        fs.writeFile(root_path + w_file, w_content, function (e) {
+            if (e) throw e;
+        })
+    }
+})
